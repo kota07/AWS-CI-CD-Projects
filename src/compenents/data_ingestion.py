a659223770a58360ddec877a599d3c26b16761ea
Teja <kotateja09@gmail.com>
2026-04-18 16:48:56 +0530

    AWS_push

--- src/compenents/data_ingestion.py
+++ src/compenents/data_ingestion.py
@@ -53,15 +53,15 @@ class DataIngestion:
             logging.info("Error occurred in data ingestion")
             raise CustomException(e, sys)   
         
-if __name__ == "__main__":
-    obj = DataIngestion()
-    train_data,test_data=obj.initiate_data_ingestion()
+# if __name__ == "__main__":
+#     obj = DataIngestion()
+#     train_data,test_data=obj.initiate_data_ingestion()
 
-    data_transformation_obj = DataTransformation()
-    train_arr,test_arr,_ = data_transformation_obj.initiate_data_transformation(train_data, test_data)
+#     data_transformation_obj = DataTransformation()
+#     train_arr,test_arr,_ = data_transformation_obj.initiate_data_transformation(train_data, test_data)
 
-    model_trainer_obj = ModelTrainer()
-    model_trainer_obj.initiate_model_trainer(train_arr, test_arr)
+#     model_trainer_obj = ModelTrainer()
+#     model_trainer_obj.initiate_model_trainer(train_arr, test_arr)
 
 
 
